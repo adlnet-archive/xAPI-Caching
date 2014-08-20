d19ea862d35374e855b14821fc45add68bee2235
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2014-08-20 16:52:14 -0400

    Added minimal server code and config file

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
-#!/bin/env nodejs
+/*
+ * Main server router
+ */
 
+var http = require('http'),
+	express = require('express'),
+	
+	config = require('./config.json');
+
+var app = express();
+
+app.get('/', function(req,res,next){
+	res.send('Hello World!');
+});
+
+http.createServer(app).listen(config.port);
 
