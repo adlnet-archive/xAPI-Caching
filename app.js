305a1ca911fb0cd17d0dc402f4b5cdd7200eab5f
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2014-08-20 17:29:35 -0400

    Added /freshen, 404 catcher

--- app.js
+++ app.js
@@ -4,13 +4,31 @@
 
 var http = require('http'),
 	express = require('express'),
+	//iso8601 = require('node-iso8601'),
 	
 	config = require('./config.json');
 
+
 var app = express();
+
+app.post('/freshen', function(req,res,next)
+{
+	if(config.manualFreshen === true)
+	{
+		res.status(200).send();
+	}
+	else {
+		res.status(403).send();
+	}
+});
 
 app.get('/', function(req,res,next){
 	res.send('Hello World!');
+});
+
+// all else fails, 404
+app.use(function(req,res){
+	res.status(404).send();
 });
 
 http.createServer(app).listen(config.port);
